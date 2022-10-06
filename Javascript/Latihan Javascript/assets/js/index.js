347ec752febd0f940d03f7af8b28c8fa0f7ee45c
ratukf <107534136+ratukf@users.noreply.github.com>
2022-10-06 15:04:31 +0700

    Add files via upload

---
+++ Javascript/Latihan Javascript/assets/js/index.js
@@ -0,0 +1,5 @@
+//document.getElementById("count-el").innerText = 5 
+//Document adalah file apa yang dituju, 
+//getElementById adalah mengambil suatu elemen dari id tertentu
+
+document.getElementById(satu).innerHTML = 'xxxxx';
